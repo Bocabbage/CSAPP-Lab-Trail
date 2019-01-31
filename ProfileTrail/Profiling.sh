0c70aa863a12a2bbd4652603337831a21d645c15
Bocabbage <378066505@qq.com>
2019-02-01 00:02:40 +0800

    Debug the 'profiling.sh'.

--- ProfileTrail/Profiling.sh
+++ ProfileTrail/Profiling.sh
@@ -2,7 +2,7 @@ path=/home/bocabbage/programming/ProfileTrail
 
 # 编译以merge1为子方法的排序
 # 可更改优化等级多次实验
-g++ -pg -O1 $path/MergeSort_prof.cpp -o merge1_O1
+g++ -pg -O1 $path/MergeSort_prof.cpp -o merge1
 # 运行(将产生gmon.out文件)
 merge1
 # Profiling
@@ -10,7 +10,7 @@ gprof -b merge1 $path/gmon.out > report1.txt
 
 # 更改源文件中子方法，重新编译
 # 编译以merge2为子方法的排序
-g++ -pg -O1 $path/MergeSort_prof.cpp -o merge2_O1
+g++ -pg -O1 $path/MergeSort_prof.cpp -o merge2
 # 运行(将产生gmon.out文件)
 merge2
 # Profiling
