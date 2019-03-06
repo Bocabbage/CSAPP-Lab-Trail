5ae910c6c8448b5fec37c83aaffd9c7462c531da
Bocabbage <378066505@qq.com>
2019-03-06 11:03:51 +0800

    Update the 'proxy.c'.(unfinished)

--- proxylab-handout/proxy.c
+++ proxylab-handout/proxy.c
@@ -4,7 +4,7 @@
                and an end server.
     
     更新时间：2019/3/4(进度：搭建程序框架--基本照搬Tiny的设计，暂未考虑多线程，处理方式的不同将在doit()体现)
-
+             2019/3/6(进度：doit接受terminal请求部分完成，向server发送请求部分完成)[均未验证]
 */
 
 #include <stdio.h>
@@ -17,7 +17,8 @@
 static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
 
 /* Functions */
-void doit(connfd);
+void doit(int connfd);
+void reqhd_rd(rio_t *rio,char* host);
 
 int main(int argc, char **argv)
 {
@@ -44,4 +45,71 @@ int main(int argc, char **argv)
         Close(connfd);
     }
     return 0;
+}
+
+void doit(int connfd)
+{
+    /* Accept and read the HTTP requests from terminal(s) */
+    char ubuf[MAXLINE]; // For requests reading
+    char method[MAXLINE],host[MAXLINE],uri[MAXLINE],version[MAXLINE];
+    rio_t lrio;
+
+    Rio_readinitb(&lrio,connfd);
+    Rio_readlineb(&lrio,ubuf,MAXLINE);
+    printf("Requests Header:\n");
+    printf("%s",ubuf);
+
+    sscanf(ubuf,"%s %s %s",method,uri,version);
+    if(strcasecmp(method,"GET"))
+    {
+        clineterror(fd,method,"501","No implemented",
+            "The TINY server doesn't implement this method");
+        return;
+    }
+    // The lab requires implementing a HTTP/1.0 GET
+    // but also asks 'Host' info from the Request headers
+    // to 'coax sensible responses out of certain Web servers'
+    // So we do need a ReadHead parsing
+    reqhd_rd(&lrio,host);
+
+    /* Send the HTTP requests to the Web Server */
+    int clientfd;
+    char *port = "8080";
+    char reqline[MAXLINE];
+    char reqhead[MAXLINE];
+    rio_t rio;
+
+    clientfd = Open_clientfd(host,port);
+    Rio_readinitb(&rio,clientfd);
+    // Build the HTTP request line
+    strcmp(reqline,"GET ");
+    strcat(reqline,host);
+    strcat(reqline,uri);
+    strcat(reqline," ");
+    strcat(reqline,version);
+    strcat(reqline,"\r\n");
+    // request head is the User-Agent
+    // request end is an "\r\n"
+
+    Rio_writen(clientfd,reqline,strlen(reqline));
+    Rio_writen(clientfd,user_agent_hdr,strlen(user_agent_hdr));
+    Rio_writen(clientfd,"\r\n",strlen("\r\n"));
+
+    
+
+}
+
+void reqhd_rd(rio_t *rio,char* host)
+{
+    char ubuf[MAXLINE];
+    Rio_readlineb(&rio,ubuf,MAXLINE);
+    // sscanf() will just replace the "\r\n" to '\0'
+    sscanf(ubuf,"%*s%s",host);
+    printf("Host: %s",host);
+    while(strcmp(ubuf,"\r\n"))
+    {
+        Rio_readlineb(&rio,ubuf,MAXLINE);
+        printf("%s",ubuf);
+    }
+    return;
 }
