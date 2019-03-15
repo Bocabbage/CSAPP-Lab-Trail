fe75f7654d369d1c03bf3e381644abf66f59de1d
Bocabbage <378066505@qq.com>
2019-03-15 21:40:10 +0800

    Update 'proxy.c'(PART-II finished).

--- proxylab-handout/proxy.c
+++ proxylab-handout/proxy.c
@@ -6,6 +6,12 @@
     更新时间：2019/3/4(进度：搭建程序框架--基本照搬Tiny的设计，暂未考虑多线程，处理方式的不同将在doit()体现)
              2019/3/6(进度：doit接受terminal请求部分完成，向server发送请求部分完成)[均未验证]
              2019/3/9(进度：完成PartI:单事务顺序处理proxy)
+             2019/3/15(进度：partII--并发完成[基于线程并发])
+*/
+
+/*
+    
+
 */
 
 #include <stdio.h>
@@ -21,14 +27,17 @@ static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64;
 void doit(int connfd);
 void reqhd_rd(rio_t *rio);
 void uri_parsing(char *uri, char* host,char* trac,char *port);
-void clienterror(int fd,char *cause,char *errnum,
-    char *shortmsg,char *longmsg);
+void *thread(void *Pconnfd);
+//void clienterror(int fd,char *cause,char *errnum,
+//    char *shortmsg,char *longmsg);
 int main(int argc, char **argv)
 {
-    int listenfd,connfd;
+    int listenfd,*connfd;
     char hostname[MAXLINE], port[MAXLINE];  // MAXLINE is defined in csapp.h
     socklen_t clientlen;
     struct sockaddr_storage clientaddr;
+
+    pthread_t tid;
 
     if(argc != 2)
     {
@@ -41,12 +50,23 @@ int main(int argc, char **argv)
     while(1)
     {
         clientlen = sizeof(clientaddr); // Keeps the code protocol-independent
-        connfd = Accept(listenfd,(SA *)&clientaddr, &clientlen);
+        connfd = Malloc(sizeof(int));
+        *connfd = Accept(listenfd,(SA *)&clientaddr, &clientlen);
         Getnameinfo((SA *)&clientaddr,clientlen,hostname,MAXLINE,port,MAXLINE,0);
-        doit(connfd);
+        Pthread_create(&tid,NULL,thread,connfd);
+        //doit(connfd);
         
     }
     return 0;
+}
+
+void *thread(void *Pconnfd)
+{
+    int connfd = *((int*)Pconnfd);
+    Pthread_detach(pthread_self());
+    Free(Pconnfd);
+    doit(connfd);
+    return NULL;
 }
 
 void doit(int connfd)
